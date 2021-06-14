72954aedb97c334138430d91b457f0dffee77572
Mehmet <mehmetkusken@gmail.com>
2021-06-13 20:13:39 -0600

    commit tree 33cc3704af65d4cdd025fb44899edf0c0e340824

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,16 +2,20 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
-
   def new
     @author = Author.new
   end
 
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
   end
+end
+
 
   private
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,21 +2,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-
   def edit
     @post = Post.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:id])
+     @post.assign_attributes(post_params)
+    if @post.valid?
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+     @post.save
+     redirect_to post_path(@post)
+   else
+     render :edit
+   end
   end
 
   private
-
   def post_params
     params.permit(:title, :category, :content)
   end
